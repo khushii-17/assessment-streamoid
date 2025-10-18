2813b6b8106600e2a4aa73f1434fbcb38ce112a7
Khushii <170603khushi@gmail.com>
2025-10-18 11:49:17 +0530

    Add Product model using Pydantic

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel
 
+class Product(BaseModel):
+    product_sku: str
+    product_name: str
+    product_brand: str
+    product_color: str
+    product_mrp: float
+    product_quantity: int
